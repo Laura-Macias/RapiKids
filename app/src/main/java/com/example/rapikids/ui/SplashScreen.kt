fc292678eddcd3fa31600f31c34ac75fc4c04acf
LauraMacias <laurma999@gmail.com>
2025-04-04 19:10:17 -0500

    Se actualiza el splash de la aplicación

--- app/src/main/java/com/example/rapikids/ui/SplashScreen.kt
+++ app/src/main/java/com/example/rapikids/ui/SplashScreen.kt
@@ -2,13 +2,19 @@ package com.example.rapikids.ui
 
 import android.os.Handler
 import android.os.Looper
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.rapikids.R
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
@@ -17,10 +23,31 @@ fun SplashScreen(navController: NavHostController) {
             navController.navigate("login") {
                 popUpTo("splash") { inclusive = true }
             }
-        }, 5000) // 5 segundos
+        }, 3000)
     }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "RapiKids", fontSize = 30.sp)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD980FA)),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = "RapiKids",
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        }
     }
 }
+
