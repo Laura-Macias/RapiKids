344d497c40669c4846bcfb73c9087f1169361987
LauraMacias <laurma999@gmail.com>
2025-06-01 16:19:00 -0500

    Se implementa una dependencia.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
-
+    implementation("androidx.fragment:fragment:1.5.7")
 
 }
